20418c75e93c6e6172fe30b5edba7cc7c0230b88
RasCarlito <rascarlito@gmail.com>
2016-09-07 10:21:49 +0200

    chore(deploy): Setup deploy using shipit

---
+++ shipitfile.js
@@ -0,0 +1,43 @@
+module.exports = function (shipit) {
+  require('shipit-deploy')(shipit)
+
+  const workspace = '/tmp/shipit/carlogren'
+
+  shipit.initConfig({
+    default: {
+      workspace: workspace,
+      deployTo: '/home/rascarlito/carlogren-test',
+      repositoryUrl: 'git@github.com:RasCarlito/carlogren.git',
+      keepReleases: 3,
+      ignores: ['.git', 'node_modules'],
+      dirToCopy: workspace + '/dist'
+    },
+    servers: {
+      production: 'rascarlito@carlogren.com'
+    }
+  })
+
+  shipit.blTask('dependencies', function () {
+    return shipit.local('cd ' + workspace + ' && npm install')
+      .then(function () {
+        shipit.log('Successfully installed dependencies')
+      })
+      .catch(function () {
+        shipit.log('Failed to install dependencies')
+      })
+  })
+
+  shipit.blTask('build', function () {
+    return shipit.local('cd ' + workspace + ' && npm run build')
+      .then(function () {
+        shipit.log('Successfully built the project')
+      })
+      .catch(function () {
+        shipit.log('Failed to build the project')
+      })
+  })
+
+  shipit.on('fetched', function () {
+    shipit.start('dependencies', 'build')
+  })
+}
